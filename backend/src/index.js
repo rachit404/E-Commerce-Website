7ed3a44062b0a03c9646b7e8f5cee81ce32157ab
rachit404 <doshirachit28@gmail.com>
2025-04-11 00:00:22 +0530

    connect to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,8 @@
-import express from "express";
-const app = express(); // Creating an express app
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-// Create a route that sends a response when visiting the homepage
-app.get("/", (req, res) => {
-  res.send("<h1>Hello, Express.js Server!</h1>");
+dotenv.config({
+  path: "./.env",
 });
 
-// Set up the server to listen on port 3000
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+connectDB();
