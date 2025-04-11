2773fed203fa4124c24fc2da41bfab2fd64c6178
rachit404 <doshirachit28@gmail.com>
2025-04-12 00:15:07 +0530

    configure cloudinary and a few other changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -16,5 +16,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true })); //* read more
 app.use(express.static("public")); //? for public folder created manually
 app.use(cookieParser());
+
+//? Routes
+import userRouter from "./routes/user.routes.js";
+app.use("/api/users", userRouter);
 
 export { app };
